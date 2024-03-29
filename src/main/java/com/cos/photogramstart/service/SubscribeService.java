fefcd4e38c7b22a01eaff45378072ac36053381c
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-03-29 15:00:04 +0900

    feat: 구독모달 정보 api 제작 & 서브쿼리 작성

--- src/main/java/com/cos/photogramstart/service/SubscribeService.java
+++ src/main/java/com/cos/photogramstart/service/SubscribeService.java
@@ -2,17 +2,45 @@ package com.cos.photogramstart.service;
 
 import com.cos.photogramstart.domain.subscribe.SubscribeRepository;
 import com.cos.photogramstart.handler.ex.CustomApiException;
-import com.cos.photogramstart.web.dto.CMRespDto;
+import com.cos.photogramstart.web.dto.subscribe.SubscribeDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.qlrm.mapper.JpaResultMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SubscribeService {
     private final SubscribeRepository subscribeRepository;
+    private final EntityManager em;
+
+    @Transactional(readOnly = true)
+    public List<SubscribeDto> subscribeList(int principalId, int pageUserId){
+        StringBuffer sb = new StringBuffer();
+        sb.append("SELECT u.id, u.username, u.profileImageUrl, ");
+        sb.append("if((SELECT 1 FROM subscribe ");
+        sb.append("WHERE fromUserId = ? AND toUserId = u.id),1,0) subscribeState, ");
+        sb.append("if((?=u.id),1,0) equalUserState ");
+        sb.append("FROM user u INNER JOIN subscribe s ");
+        sb.append("ON u.id = s.toUserId ");
+        sb.append("WHERE s.fromUserId = ?");
+
+        Query query = em.createNativeQuery(sb.toString())
+                .setParameter(1, principalId)
+                .setParameter(2, principalId)
+                .setParameter(3, pageUserId);
+
+        // 쿼리 실행 단계
+        JpaResultMapper result = new JpaResultMapper();
+        List<SubscribeDto> subscribeDtos =  result.list(query, SubscribeDto.class);
+
+        return subscribeDtos;
+    }
+
     @Transactional
     public void subscribe(int fromUserId, int toUserId){
         try{
