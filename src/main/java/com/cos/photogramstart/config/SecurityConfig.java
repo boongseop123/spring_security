6b58aa77f4fcaf2a53ab3f69aa8c03f0e6d9301e
jihoon98 <tlswlgns1003@sju.ac.kr>
2024-04-02 19:58:11 +0900

    refactor: oauth 오류 해결

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.photogramstart.config;
 
+import com.cos.photogramstart.config.oauth.OAuth2DetailsService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,25 +9,38 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableWebSecurity//해당 파일로 시큐리티를 활성화
-@Configuration//IOC
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@EnableWebSecurity // 해당 파일로 시큐리티를 활성화 시키는 어노테이션
+@Configuration // IoC컨테이너로 넣어주는 어노테이션
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final OAuth2DetailsService oAuth2DetailsService;
+
     @Bean
-    public BCryptPasswordEncoder encode(){
+    public BCryptPasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        //super 삭제->기존 시큐리티가 가지고 있는 기능이 다 비활성화
-        http.csrf().disable();//postman이던, 홈페이지던 구분하지 않겠다.(csrf)비활성화
-        http.authorizeRequests()
-                .antMatchers("/","/users/**","/image/**","/subscribe/**","/comment/**","/api/**").authenticated()//이 주소는 인증이 필요
-                .anyRequest().permitAll()//이 주소는 다 허용
+    protected void configure(HttpSecurity http) throws Exception {
+        // TODO Auto-generated method stub
+
+        http.csrf().disable() // CSRF토큰 검사를 비활성화 하겠다.
+                .authorizeRequests() // 이 주소경로로 요청이 들어오면
+                .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**", "/api/**")
+                .authenticated() // 인증이 필요하다.
+                .anyRequest() // 그 외의 요청들은
+                .permitAll() // 모두 허용한다.
+                .and() // 그리고
+                .formLogin() // 로그인(인증)이 필요한 요청이 들어오면
+                .loginPage("/auth/signin") // 로그인페이지 auth/signin 으로 이동시키고(GET요청)
+                .loginProcessingUrl("/auth/signin") // auth/signin 이라는 POST요청을 실행시킨다.
+                .defaultSuccessUrl("/") // 인증이 정삭적으로 완료되면 / 로 이동한다.
                 .and()
-                .formLogin()//인증이 필요한 페이지에 접속을 시도하면
-                .loginPage("/auth/signin")//Get
-                .loginProcessingUrl("/auth/signin")//Post-->스프링시큐리티가 로그인프로세스 진행
-                .defaultSuccessUrl("/");//로그인을 정상적으로 하면 이 url로 오도록
+                .oauth2Login() // form로그인 외에 OAuth2 로그인도 인정하겠다.
+                .userInfoEndpoint() // OAuth2 로그인 인증시 인증코드가 아닌 회원정보를 바로 받겠다.
+                .userService(oAuth2DetailsService); // 회원정보를 담을 서비스 로직 설정영역.
     }
 }
